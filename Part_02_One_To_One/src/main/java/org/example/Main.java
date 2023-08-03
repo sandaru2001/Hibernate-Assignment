00e2af0c70d7ada95bb2c77ddbbfd156fbed0f42
Sandaru Shehan <sandarushehan96@gmail.com>
2023-08-04 01:01:09 +0530

    One to One Relationship Edited

--- Part_02_One_To_One/src/main/java/org/example/Main.java
+++ Part_02_One_To_One/src/main/java/org/example/Main.java
@@ -10,7 +10,7 @@ public class Main {
     public static void main(String[] args) {
         Book book = new Book();
         book.setBookId("B001");
-        book.setBookName("lushan ponnaya");
+        book.setBookName("The Witcher");
 
         Author author = new Author();
         author.setAuthorId("A001");
